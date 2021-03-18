d54538008cb01a60eb22274f4375eaed69723745
xocer <vitamin9292@mail.ru>
2021-03-18 13:11:05 +0300

    Конфигурация Hibernate

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,24 @@
 package ru.job4j.tracker;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "items")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 
+    public Item() {
+    }
+
     public Item(int id, String name) {
         this.id = id;
+        this.name = name;
+    }
+
+    public Item(String name) {
         this.name = name;
     }
 
@@ -23,5 +36,13 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
